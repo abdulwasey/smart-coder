7dcd533f61edd77daad2b4bf0bd230d8b218328c
Abdul Wasey <abdulwasey13@gmail.com>
2022-04-20 05:31:03 +0530

    LC-1

---
+++ leetcode/1.two-sum.ts
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (target === nums[i] + nums[j]) {
+        return [i, j];
+      }
+    }
+  }
+}
+// @lc code=end
